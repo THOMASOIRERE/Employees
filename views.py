16e95665f31e9372d8542d1d5875fe2f7e2fab2c
THOMASOIRERE <101627410+THOMASOIRERE@users.noreply.github.com>
2022-04-14 15:49:48 +0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,23 @@
+from email import message
+from django.shortcuts import render
+from CRUDOperation.models import EmpModel
+from django.contrib import messages
+
+def showemp(request):
+    showall=EmpModel.objects.all()
+    return render(request,'index.html',{"data":showall})
+
+def Insertemp(request):
+    if request.method=="POST":
+        if request.POST.get('empname') and request.POST.get('email') and request.POST.get('occupation') and request.POST.get('salary') and request.POST.get('gender'):
+            saverecord=EmpModel()
+            saverecord.empname=request.POST.get('empname')
+            saverecord.email=request.POST.get('email')
+            saverecord.occupation=request.POST.get('occupation')
+            saverecord.salary=request.POST.get('salary')
+            saverecord.gender=request.POST.get('gender')
+            saverecord.save()
+            messages.success(request,'student '+saverecord.empname+ 'Is saved successfully..!')
+            return render(request,'Insert.html')
+        else:
+              return render(request,'Insert.html')  
